85de7f333b053af0b47ca1b2cdec85ce1a55dcaa
jaylenperez <jaylenestella24@gmail.com>
2025-04-04 16:09:20 -0500

    Connected database, ran sql script to create tables, added data folder which includes csv data files

--- create_db.sql
+++ create_db.sql
@@ -1,6 +1,7 @@
 -- Create Episode Table
 CREATE TABLE episodes (
     episode_id SERIAL PRIMARY KEY,
+    -- INT -> +1
     title VARCHAR(255) NOT NULL,
     season INT NOT NULL,
     episode_number INT NOT NULL,
@@ -8,20 +9,17 @@ CREATE TABLE episodes (
     air_date DATE,
     img_src VARCHAR(255)
 );
-
 -- Create Colors Table
 CREATE TABLE colors (
     color_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE,
     hex_value VARCHAR(7) NOT NULL UNIQUE
 );
-
 -- Create Subjects Table
 CREATE TABLE subjects (
     subject_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE
 );
-
 -- Create Episodes & Colors Join Table
 CREATE TABLE episode_colors (
     episode_color_id SERIAL PRIMARY KEY,
@@ -30,7 +28,6 @@ CREATE TABLE episode_colors (
     FOREIGN KEY (episode_id) REFERENCES episodes(episode_id),
     FOREIGN KEY (color_id) REFERENCES colors(color_id)
 );
-
 -- Create Episodes & Subject Matter Join Table
 CREATE TABLE episode_subjects (
     episode_subjects_id SERIAL PRIMARY KEY,
