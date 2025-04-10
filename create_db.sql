d87b6e1b0449ee5aae698da43e6421c2f9ee5758
jaylenperez <jaylenestella24@gmail.com>
2025-04-09 21:03:49 -0500

    deleted old tables and created new ones

--- create_db.sql
+++ create_db.sql
@@ -9,34 +9,35 @@ CREATE TABLE episodes (
 );
 -- Create Colors Table
 CREATE TABLE colors (
-    id PRIMARY KEY,
+    id INT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     season INT NOT NULL,
     episode INT NOT NULL,
     img_src TEXT,
     youtube_src TEXT,
-    colors TEXT [],
-    color_hex TEXT []
+    colors TEXT,
+    color_hex TEXT
 );
 -- Create Subjects Table
 CREATE TABLE subjects (
-    subject_id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    subject TEXT []
+    subject TEXT
 );
 -- Create Episodes & Colors Join Table
 CREATE TABLE episode_colors (
-    episode_color_id PRIMARY KEY,
     episode_id INT NOT NULL,
     color_id INT NOT NULL,
-    FOREIGN KEY (episode_id) REFERENCES episodes(episode_id),
-    FOREIGN KEY (color_id) REFERENCES colors(color_id)
+    FOREIGN KEY (episode_id) REFERENCES episodes(id),
+    FOREIGN KEY (color_id) REFERENCES colors(id)
 );
 -- Create Episodes & Subject Matter Join Table
 CREATE TABLE episode_subjects (
-    episode_subjects_id SERIAL PRIMARY KEY,
     episode_id INT NOT NULL,
     subject_id INT NOT NULL,
-    FOREIGN KEY (episode_id) REFERENCES episodes(episode_id),
-    FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
+    FOREIGN KEY (episode_id) REFERENCES episodes(id),
+    FOREIGN KEY (subject_id) REFERENCES subjects(id)
 );
+-- EDIT EPISODES TABLE
+ALTER TABLE episodes
+ALTER COLUMN notes TYPE VARCHAR(255);
