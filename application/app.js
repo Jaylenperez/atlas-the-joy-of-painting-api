b59b90e530470e06cc4fe7e10690b01f1b8d3eb0
jaylenperez <jaylenestella24@gmail.com>
2025-04-12 12:46:21 -0500

    updated app.js to serve static files

--- application/app.js
+++ application/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const path = require('path');
 const episodesRouter = require('./routes/episodes.routes');
 const { errorHandler } = require('./middlewares/errorhandler');
 dotenv.config();
 
 app.use(express.json());
+
+app.use(express.static('public'));
 
 app.use('/api/episodes', episodesRouter);
 app.use(errorHandler);
